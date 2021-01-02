a80d3b4f0e0d9da63d4af5bd9c4e64ef151c8e6e
jval1972 <jvalavanis@gmail.com>
2021-01-02 14:16:52 +0200

    Updated year in copyright notice

--- frm_texture.pas
+++ frm_texture.pas
@@ -1,7 +1,7 @@
 //------------------------------------------------------------------------------
 //
 //  DD_MODEL: DelphiDOOM Procedural Model Editor
-//  Copyright (C) 2017 by Jim Valavanis
+//  Copyright (C) 2017-2021 by Jim Valavanis
 //
 //  This program is free software; you can redistribute it and/or
 //  modify it under the terms of the GNU General Public License
